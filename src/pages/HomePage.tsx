72ba9c0165c9b157a8d063e0ac27ab91cbc2b395
adrisa <adsyarif28@gmail.com>
2024-11-21 15:02:12 +0700

    feat: add header

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,47 @@
+const HomePage = () => {
+  interface category {
+    id: number;
+    title: string;
+  }
+
+  const categories: category[] = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <>
+      {/* <div className="categories-container">
+        {categories.map((category) => (
+          <div className="category-container">
+            {<img />}
+            <div key={category.id} className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop now</p>
+            </div>
+          </div>
+        ))}
+      </div> */}
+    </>
+  );
+};
+
+export default HomePage;
