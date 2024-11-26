bb2e52443e224555c8fad8f5399b535b26dd767c
adrisa <adsyarif28@gmail.com>
2024-11-27 02:30:47 +0700

    fix: home page padding

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="font-sans mt-7 pt-16 pb-[32px] md:mt-0">
+    <div className="font-sans mt-7 pt-16 md:mt-0">
       <Header />
       <main>{children}</main>
       <Footer />
