426cbc5e9223eb17660c38ce727dff1c6e60f801
adrisa <adsyarif28@gmail.com>
2024-11-26 12:39:46 +0700

    fix: fix layout shifting in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -41,11 +41,11 @@ const HomePage = () => {
   return (
     <>
       <div className="flex flex-col justify-center mt-7 pt-16 pb-[32px] md:mt-0">
-        <div className="flex items-center justify-center bg-[url('./bg-1.jpg')] bg-center w-full h-[350px] object-cover bg-cover md:h-[450px] lg:h-[600px] lg:bg-no-repeat">
-          <div className="p-4 w-full md:p-28 md:w-3/4">
-            <div className="flex flex-col gap-3 items-start">
+        <div className="flex p-4 items-center justify-center bg-[url('./bg-1.jpg')] bg-center w-full h-[350px] object-cover bg-cover md:h-[450px] lg:h-[600px] lg:bg-no-repeat">
+          <div className="p-4 w-full xl:p-28 xl:w-3/4">
+            <div className="flex flex-col gap-6 items-start">
               <p>Brand</p>
-              <h1 className="text-3xl font-light md:text-5xl">New Colection</h1>
+              <h1 className="text-3xl font-light md:text-7xl">New Colection</h1>
               <button className="bg-[#0315ff] py-4 px-16 text-white mt-5">
                 Shop Now!
               </button>
@@ -53,14 +53,14 @@ const HomePage = () => {
           </div>
         </div>
         <div className="flex items-center justify-center w-full">
-          <div className="px-4 py-20 w-full flex flex-wrap justify-center  md:w-3/4">
+          <div className="py-20 w-full flex flex-wrap justify-center">
             {categories.map((category) => {
               console.log(category.imgUrl);
               return (
                 <>
-                  <div className="md:p-4 md:w-[240px] xl:w-[350px] xl:h-[240px]">
+                  <div className="w-full md:w-1/3 md:h-[240px] lg:px-4 xl:w-[350px]">
                     <div
-                      className={`flex justify-center items-center container w-full h- border`}
+                      className={`flex justify-center items-center container w-full h-[180px] md:w-[300px]`}
                       style={{
                         backgroundImage: `url(${category.imgUrl})`,
                         backgroundSize: "cover",
@@ -82,16 +82,18 @@ const HomePage = () => {
             })}
           </div>
         </div>
-        <div className="p-4 w-full flex justify-center">
-          <div className="flex justify-between items-center p-4 w-full h-96 bg-[url(/bg-5.jpg)] bg-center bg-no-repeat bg-cover md:w-3/4">
+        <div className="px-4 w-full flex justify-center">
+          <div className="flex justify-between items-center p-4 w-full h-96 bg-[url(/bg-5.jpg)] bg-center bg-no-repeat bg-cover md:h-[550px] xl:w-3/4">
             <div className="w-1/2 lg:w-7/12"></div>
-            <div className="w-1/2 flex flex-col items-end lg:items-start lg:w-5/12">
+            <div className="w-1/2 flex flex-col items-end md:items-start xl:w-5/12">
               <div>
-                <div className="flex flex-col gap-3 ">
+                <div className="flex flex-col gap-3">
                   <p className="text-red-600 text-xl font-bold lg:text-2xl">
                     -60%
                   </p>
-                  <p className="text-2xl font-bold lg:text-4xl">Global Sale</p>
+                  <p className="text-2xl font-bold md:text-5xl lg:text-6xl">
+                    Global Sale
+                  </p>
                 </div>
                 <button className="text-sm bg-[#0315ff] py-4 px-8 text-white mt-12 h-[50px] min-w-[170px]">
                   BUY NOW
