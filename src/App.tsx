c1bf781f50c0c629ac9b241a71ce6a40e8004e6b
adrisa <adsyarif28@gmail.com>
2024-11-28 16:58:08 +0700

    feat: add register page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/Auth/RegisterPage";
+import { LoginPage, RegisterPage } from "./pages/Auth";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
